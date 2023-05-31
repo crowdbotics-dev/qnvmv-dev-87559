fe5830584f8a0ef535cc222c98f71cd101920955
Crowdbotics <engineering@crowdbotics.com>
2023-05-31 17:49:22 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/qnvmv_dev_87559/MainActivity.java
@@ -0,0 +1,15 @@
+package com.qnvmv_dev_87559;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "qnvmv_dev_87559";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "qnvmv-dev-87559",
+  url: "https://qnvmv-dev-87559.botics.co",
+  api: "https://qnvmv-dev-87559.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
